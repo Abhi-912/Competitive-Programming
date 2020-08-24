6ca39a751a672e2811c63297f139d588354a07e1
Abhi-912 <56739045+Abhi-912@users.noreply.github.com>
2020-08-25 00:30:51 +0530

    Update SieveofErastothenes.cpp

--- SieveofErastothenes.cpp
+++ SieveofErastothenes.cpp
@@ -1,15 +1,4 @@
-#include<bits/stdc++.h>
-#define SPEED ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define ll long long
-#define ld long double
-#define fi(i,j,n) for(int i=j;i<n;i++)
-#define fd(i,j,n) for(int i=j;i>n;i--)
-#define debug(n1) cout<<n1<<endl;
-#define pb push_back
-#define eb emplace_back
-#define vi vector<int>
-#define mod 1e9+7
-using namespace std;
+
 void sieveoferast(int n){
     bool prime[n+1];
     memset(prime,true,sizeof(prime));
@@ -18,23 +7,14 @@ void sieveoferast(int n){
             for(int i=p*p;i<=n;i+=p) prime[i]=false;
         }
     }
-    /*for(int i=2;i<=n;i++){
+    for(int i=2;i<=n;i++){
         if(prime[i]) cout<<prime[i]<<" ";
     }
-    cout<<endl;*/
+    cout<<endl;
 }
 int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        //freopen("output.txt","w",stdout);
-    #endif
-SPEED;
-int t;
-cin>>t;
-while(t--){
 int n;
 cin>>n;
 sieveoferast(n);
-}
 return 0;
 }
