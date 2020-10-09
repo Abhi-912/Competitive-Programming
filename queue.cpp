75406f2e228b8ff0259bbf98779d976011f05916
Abhishek pandey <56739045+Abhi-912@users.noreply.github.com>
2020-10-09 14:15:24 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,50 @@
+struct Node{
+    int data;
+    Node *next;
+};
+class mqueue{
+    public:
+    Node *front,*rear;
+    mqueue(){front=rear=nullptr;}
+    void push(int n);
+    void pop();
+    void display();
+    ~mqueue();
+};
+void mqueue::push(int n){
+    Node *temp=new Node;
+    if(temp==nullptr) {cout<<"OVERFLOW"<<endl;return ;}
+    temp->data=n;
+    temp->next=nullptr;
+    if(front==nullptr) front=rear=temp;
+    else{
+        rear->next=temp;
+        rear=temp;
+    }
+}
+void mqueue::pop(){
+    if(front==nullptr) {cout<<"QUEUE IS EMPTY"<<endl;return;}
+    else{
+        if(front=rear) front=rear=nullptr;
+        else{
+            front=front->next;
+        } 
+    }
+}
+mqueue::~mqueue(){
+    while(front!=nullptr){
+        Node *temp=front;
+        front=front->next;
+        delete temp;
+    }
+    rear=nullptr;
+}
+void mqueue::display(){
+    if(front==nullptr) {cout<<"EMPTY"<<endl;return;}
+        Node *temp=front;
+        while(temp){
+            cout<<temp->data;
+            temp=temp->next;
+    }
+    cout<<endl;
+}
