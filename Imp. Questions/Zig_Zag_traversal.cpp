f269c8ef00277a5bd5f907c22a45764337505d14
Abhishek pandey <56739045+Abhi-912@users.noreply.github.com>
2021-05-21 04:13:35 +0530

    Create Zig_Zag_traversal.cpp

---
+++ Imp. Questions/Zig_Zag_traversal.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> vv;
+        if(root == NULL) return vv;
+        stack<TreeNode*> s1, s2;
+        s1.push(root);
+        vector<int> v;
+        while(!s1.empty() || !s2.empty()) {
+            while(!s1.empty()) {
+                
+                TreeNode* temp;
+                temp = s1.top();
+                if(temp != NULL) {
+                v.push_back(temp->val);
+                s1.pop();
+                if(temp ->left != NULL) s2.push(temp->left);
+                if(temp->right != NULL) s2.push(temp->right);
+                }
+            }
+            if(v.size()!=0) {
+            vv.push_back(v);
+            v.clear();
+            }
+            while(!s2.empty()) {
+                TreeNode* temp;
+                temp = s2.top();
+                if(temp != NULL) {
+                v.push_back(temp->val);
+                s2.pop();
+                if(temp->right != NULL) s1.push(temp->right);
+                if(temp->left != NULL) s1.push(temp->left);
+                }
+            }
+            if(v.size()!=0) {
+            vv.push_back(v);
+            v.clear();
+            }
+        }
+        return vv;
+    }
+};
+
+//Reference:- https://www.youtube.com/watch?v=iJT9JrrFQ44&list=PLNxqWc8Uj2LRbsOlBiPJZAyZpaUwdDepd&index=6
